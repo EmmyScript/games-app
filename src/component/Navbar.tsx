6cffd06dfca9e8e0537cd58ffe147d6f94861837
Idoko Emmanuel <idokoemma.ogbole@gmail.com>
2023-08-19 18:31:32 +0100

    build a navbar

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,14 @@
+
+import { HStack} from '@chakra-ui/react'
+
+import Logo from './Logo';
+const Navbar = () => {
+  return (
+    <HStack>
+      <Logo />
+    <text>logo</text> 
+    </HStack>
+  )
+}
+
+export default Navbar
